bd9916f88f53b52a869778e1649c7d9ed594d5af
templth <templth@570b5b7a-14bf-11de-9308-ab3afd9c0aba>
2009-06-30 16:16:12 +0000

    Updated launching parameters
    git-svn-id: http://springdm-in-action.googlecode.com/svn/trunk@58 570b5b7a-14bf-11de-9308-ab3afd9c0aba

--- ch07/container/startEquinoxEclipseLink.sh
+++ ch07/container/startEquinoxEclipseLink.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -jar org.eclipse.osgi-3.5.0.v20081201-1815.jar -configuration configuration/configuration-eclipselink
+java -jar org.eclipse.osgi-3.5.0.v20081201-1815.jar -Dosgi.framework.extensions=org.eclipse.equinox.weaving.hook -D org.aspectj.osgi.verbose=true -configuration configuration/configuration-eclipselink
