ea7461d3ca037036b20604340d22c8544b9b0108
templth <templth@570b5b7a-14bf-11de-9308-ab3afd9c0aba>
2009-06-30 14:25:04 +0000

    Updated copy script
    git-svn-id: http://springdm-in-action.googlecode.com/svn/trunk@56 570b5b7a-14bf-11de-9308-ab3afd9c0aba

--- ch07/container/copyJarEclipseLink.sh
+++ ch07/container/copyJarEclipseLink.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#cp ../com.springsource.com.mchange.v2.c3p0.config/target/ bundles/bundles-eclipselink/
+cp ../com.springsource.com.mchange.v2.c3p0.config/target/com.springsource.com.mchange.v2.c3p0.config-1.0-SNAPSHOT.jar bundles/bundles-eclipselink/
 cp ../org.springframework.osgi.log4j.config/target/org.springframework.osgi.log4j.config-1.0-SNAPSHOT.jar bundles/bundles-eclipselink/
 cp ../ch07-jdbc-datasource/target/ch07-jdbc-datasource-1.0-SNAPSHOT.jar bundles/bundles-eclipselink/
 cp ../ch07-dataaccess-simple/target/ch07-dataaccess-simple-1.0-SNAPSHOT.jar bundles/bundles-eclipselink/
