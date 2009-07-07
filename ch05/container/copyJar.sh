ee2426d159ccd8a584f4f8982372a6f1a1de1520
templth <templth@570b5b7a-14bf-11de-9308-ab3afd9c0aba>
2009-07-07 14:55:17 +0000

    Added bundles to copy
    git-svn-id: http://springdm-in-action.googlecode.com/svn/trunk@73 570b5b7a-14bf-11de-9308-ab3afd9c0aba

--- ch05/container/copyJar.sh
+++ ch05/container/copyJar.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 cp ../ch05-osgi-tccl/target/osgi-tccl-1.0-SNAPSHOT.jar bundles/
+cp ../ch05-exporting-service-simple/target/exporting-services-1.0-SNAPSHOT.jar bundles/
+cp ../ch05-referencing-service-xml-simple/target/referencing-services-xml-1.0-SNAPSHOT.jar bundles/
+cp ../ch05-referencing-service-annotation-simple/target/referencing-services-annotation-1.0-SNAPSHOT.jar bundles/
