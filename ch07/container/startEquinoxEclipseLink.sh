c53114b107539387f9af4c7fbfda0058819b84bf
templth <templth@570b5b7a-14bf-11de-9308-ab3afd9c0aba>
2009-08-06 14:23:24 +0000

    Removed projects
    git-svn-id: http://springdm-in-action.googlecode.com/svn/trunk@106 570b5b7a-14bf-11de-9308-ab3afd9c0aba

--- ch07/container/startEquinoxEclipseLink.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -jar org.eclipse.osgi-3.5.0.v20081201-1815.jar -Dosgi.framework.extensions=org.eclipse.equinox.weaving.hook -D org.aspectj.osgi.verbose=true -configuration configuration/configuration-eclipselink
--- ch07/database/startServer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export HSQLDB_LIB=com.springsource.org.hsqldb-1.8.0.9.jar
-
-java -classpath $HSQLDB_LIB org.hsqldb.Server -port 9001 -database.0 library
