dcaecb43518d2404fae7b2ae13f0054e61ee10a0
templth <templth@570b5b7a-14bf-11de-9308-ab3afd9c0aba>
2009-08-07 09:35:30 +0000

    Added script to configure projects for Eclipse
    git-svn-id: http://springdm-in-action.googlecode.com/svn/trunk@164 570b5b7a-14bf-11de-9308-ab3afd9c0aba

---
+++ ch07/configure-eclipse.sh
@@ -0,0 +1,48 @@
+cd ch07-dataaccess-simple
+/home/templth/developpement/applications/apache-maven-2.0.9/bin/mvn eclipse:eclipse
+cd -
+
+cd ch07-hibernate-integration-tests
+/home/templth/developpement/applications/apache-maven-2.0.9/bin/mvn eclipse:eclipse
+cd -
+
+cd ch07-hibernate-simple
+/home/templth/developpement/applications/apache-maven-2.0.9/bin/mvn eclipse:eclipse
+cd -
+
+cd ch07-jdbc-datasource
+/home/templth/developpement/applications/apache-maven-2.0.9/bin/mvn eclipse:eclipse
+cd -
+
+cd ch07-jpa-eclipselink-integration-tests
+/home/templth/developpement/applications/apache-maven-2.0.9/bin/mvn eclipse:eclipse
+cd -
+
+cd ch07-jpa-eclipselink-simple
+/home/templth/developpement/applications/apache-maven-2.0.9/bin/mvn eclipse:eclipse
+cd -
+
+cd ch07-jpa-hibernatejpa-integration-tests
+/home/templth/developpement/applications/apache-maven-2.0.9/bin/mvn eclipse:eclipse
+cd -
+
+cd ch07-jpa-hibernatejpa-simple
+/home/templth/developpement/applications/apache-maven-2.0.9/bin/mvn eclipse:eclipse
+cd -
+
+cd ch07-jpa-openjpa-integration-tests
+/home/templth/developpement/applications/apache-maven-2.0.9/bin/mvn eclipse:eclipse
+cd -
+
+cd ch07-jpa-openjpa-simple
+/home/templth/developpement/applications/apache-maven-2.0.9/bin/mvn eclipse:eclipse
+cd -
+
+cd com.springsource.com.mchange.v2.c3p0.config
+/home/templth/developpement/applications/apache-maven-2.0.9/bin/mvn eclipse:eclipse
+cd -
+
+cd org.springframework.osgi.log4j.config
+/home/templth/developpement/applications/apache-maven-2.0.9/bin/mvn eclipse:eclipse
+cd -
+
