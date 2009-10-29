f43c19403b6e4c8ef0edefbaa4574923a3dcb614
acogoluegnes <acogoluegnes@570b5b7a-14bf-11de-9308-ab3afd9c0aba>
2009-10-29 21:19:00 +0000

    added brand new ch. 03 samples (for Spring DM 2.0.0.M1)
    git-svn-id: http://springdm-in-action.googlecode.com/svn/trunk@230 570b5b7a-14bf-11de-9308-ab3afd9c0aba

---
+++ ch03/springdm-sample/src/main/java/com/manning/sdmia/SpringDmSample.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.manning.sdmia;
+
+/**
+ * @author acogoluegnes
+ * 
+ */
+public class SpringDmSample {
+
+	public SpringDmSample() {
+		System.out.println("Spring DM sample created");
+	}
+
+}
