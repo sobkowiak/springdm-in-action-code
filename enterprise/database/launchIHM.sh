58fb824c5efde4a9ea54750aea803f802d6a3af1
acogoluegnes <acogoluegnes@570b5b7a-14bf-11de-9308-ab3afd9c0aba>
2010-01-06 08:34:30 +0000

    removed enterprise app.
    git-svn-id: http://springdm-in-action.googlecode.com/svn/trunk@289 570b5b7a-14bf-11de-9308-ab3afd9c0aba

--- enterprise/database/launchIHM.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp com.springsource.org.hsqldb-1.8.0.9.jar org.hsqldb.util.DatabaseManager -url jdbc:hsqldb:hsql://localhost/enterprise -driver org.hsqldb.jdbcDriver -user sa
--- enterprise/enterprise-web/src/main/webapp/WEB-INF/jsp/users.jsp
+++
@@ -1,6 +0,0 @@
-<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-
-
-<c:forEach items="${users}" var="user">
-	<p>${user.firstname} ${user.lastname}</p>
-</c:forEach>
