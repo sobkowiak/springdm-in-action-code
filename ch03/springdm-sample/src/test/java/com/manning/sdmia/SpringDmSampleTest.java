d73f44df509d29bd971a98338b69e8f53e941417
acogoluegnes <acogoluegnes@570b5b7a-14bf-11de-9308-ab3afd9c0aba>
2010-04-04 18:30:18 +0000

    ch. 03: upgraded standard sample to Spring 3.0.2 as 3.0.RC1 is no longer in Maven 2 repo. Implies changing the test framework bundles.
    git-svn-id: http://springdm-in-action.googlecode.com/svn/trunk@293 570b5b7a-14bf-11de-9308-ab3afd9c0aba

--- ch03/springdm-sample/src/test/java/com/manning/sdmia/SpringDmSampleTest.java
+++ ch03/springdm-sample/src/test/java/com/manning/sdmia/SpringDmSampleTest.java
@@ -4,6 +4,8 @@
 package com.manning.sdmia;
 
 import org.osgi.framework.Bundle;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.osgi.test.AbstractConfigurableBundleCreatorTests;
 
 public class SpringDmSampleTest extends AbstractConfigurableBundleCreatorTests {
@@ -24,6 +26,13 @@ public class SpringDmSampleTest extends AbstractConfigurableBundleCreatorTests {
 
 	@Override
 	protected String[] getTestBundlesNames() {
-		return new String[] { "com.manning.sdmia, springdm-sample, 1.0-SNAPSHOT" };
+		return new String[] { "com.manning.sdmia, springdm-sample, 1.0.0" };
 	}
+	
+	@Override
+	protected Resource getTestingFrameworkBundlesConfiguration() {
+		return new InputStreamResource(
+				SpringDmSampleTest.class.getResourceAsStream("boot-bundles.properties"));
+	}	
+	
 }
